d3b37265529e1cbf77e6a503361dd7862335fabc
Kev-BB <118001660+Kev-BB@users.noreply.github.com>
2024-01-30 18:04:48 +0300

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Math-a-matik',
+  description: 'Math Quiz',
+}
+
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
